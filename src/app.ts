e4652592f46c7246d714af90bb176749e5a82124
Felipe Srutkoske <srutkoske.style@gmail.com>
2025-04-05 17:32:37 -0300

    feat: estrutura funcionando para usuários e caminhões

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import usuarioRoutes from "./Routes/Usuario";
+import caminhaoRoutes from "./Routes/Caminhao"; // <- novo import da rota de caminhões
 
 const app = express();
 
-app.use(express.json({ limit: "50mb"}));
+app.use(express.json({ limit: "50mb" }));
 
 app.use("/usuarios", usuarioRoutes);
+app.use("/caminhoes", caminhaoRoutes); // <- adicionando a rota dos caminhões
 
-app.listen(3000, () => console.log("Servidor iniciado"));
-
+app.listen(3000, () => console.log("Servidor iniciado na porta 3000"));
